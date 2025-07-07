b81ef58210c7983f8a4d2e6ff9eda828233ca9da
Gerin-Benoit <benoit.gerin@uclouvain.be>
2025-07-07 16:34:09 +0200

    bug fix wandb

--- dinov2/data/augmentations.py
+++ dinov2/data/augmentations.py
@@ -146,13 +146,8 @@ class CropForegroundSwapSliceDims(CropForeground):
         img = img.permute(*perm)
 
         # crop foreground
-        img = super().__call__(img, mode, lazy, **pad_kwargs)
-        if img.shape[-1] == 0:
-            print("=" * 50)
-            print(img.shape)
-            print(img_dict['image'])
-            exit()
-        return img
+        return super().__call__(img, mode, lazy, **pad_kwargs)
+
 
 
 class DataAugmentationDINO3d(object):
