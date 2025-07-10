8e5505ce1a6e4032bc6302a65506d80b1e13c6c1
Gerin-Benoit <benoit.gerin@uclouvain.be>
2025-07-10 15:34:21 +0200

    submitit first try

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -48,8 +48,8 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/submitit/").is_dir():
-        p = Path(f"/submitit/{user}/experiments")
+    if Path("/gpfs/home/acad/ucl-elen/gerinb/submitit/").is_dir():
+        p = Path(f"/gpfs/home/acad/ucl-elen/gerinb/submitit/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
