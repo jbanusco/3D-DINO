4feac1750b73d9e2dbfad6ab330e214e7330a6dd
Gerin-Benoit <benoit.gerin@uclouvain.be>
2025-07-23 09:34:56 +0200

    jepa implem

--- dinov2/train/ssl_meta_arch.py
+++ dinov2/train/ssl_meta_arch.py
@@ -12,7 +12,7 @@ import torch
 from torch import nn
 
 from dinov2.loss import DINOLoss, iBOTPatchLoss, KoLeoLoss
-from dinov2.models import build_model_from_cfg
+from dinov2.models import build_model_from_cfg, ijepa_build_model_from_cfg
 from dinov2.layers import DINOHead
 from dinov2.utils.utils import has_batchnorms
 from dinov2.utils.param_groups import get_params_groups_with_decay, fuse_params_groups
