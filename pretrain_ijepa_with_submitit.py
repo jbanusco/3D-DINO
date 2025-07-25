c2d931c03ca8cfc7acb9e72a962a143394433a73
Gerin-Benoit <benoit.gerin@uclouvain.be>
2025-07-25 09:40:02 +0200

    for lucia

--- pretrain_ijepa_with_submitit.py
+++ pretrain_ijepa_with_submitit.py
@@ -27,7 +27,7 @@ import submitit
 def parse_args():
     parser = argparse.ArgumentParser("Submitit for IJEPA", parents=[ijepa_train3d.get_args_parser()], add_help=False)
     parser.add_argument("--ngpus", default=4, type=int, help="Number of gpus to request on each node")
-    parser.add_argument("--nodes", default=2, type=int, help="Number of nodes to request")
+    parser.add_argument("--nodes", default=1, type=int, help="Number of nodes to request")
     parser.add_argument("--timeout", default=2800, type=int, help="Duration of the job")
     #parser.add_argument("--output_dir", default="", type=str)
 
