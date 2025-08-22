e5a880517643cfcee2618ac75ae4c5e48faac5e9
jaume <jaume@hos70292.intranet.chuv>
2025-08-22 19:34:25 +0200

    ADD: Test linear patch

--- dinov2/eval/linear3d_reg.py
+++ dinov2/eval/linear3d_reg.py
@@ -183,6 +183,21 @@ def remove_ddp_wrapper(m: nn.Module) -> nn.Module:
     return m.module if has_ddp_wrapper(m) else m
 
 
+def create_linear_input(x_tokens_list, use_n_blocks, use_avgpool):
+    intermediate_output = x_tokens_list[-use_n_blocks:]
+    output = torch.cat([class_token for _, class_token in intermediate_output], dim=-1)
+    if use_avgpool:
+        output = torch.cat(
+            (
+                output,
+                torch.mean(intermediate_output[-1][0], dim=1),  # patch tokens
+            ),
+            dim=-1,
+        )
+        output = output.reshape(output.shape[0], -1)
+    return output.float()
+
+
 def _pad_and_collate(batch):
     maxlen = max(len(targets) for image, targets in batch)
     padded_batch = [
