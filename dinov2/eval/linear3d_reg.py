5128bd7f46a56367f19e3ac48c8c02f1812a22d8
jaume <jaume@hos70292.intranet.chuv>
2025-08-22 19:32:22 +0200

    ADD: Test linear patch

--- dinov2/eval/linear3d_reg.py
+++ dinov2/eval/linear3d_reg.py
@@ -191,21 +191,6 @@ def _pad_and_collate(batch):
     return torch.utils.data.default_collate(padded_batch)
 
 
-def create_linear_input(x_tokens_list, use_n_blocks, use_avgpool):
-    intermediate_output = x_tokens_list[-use_n_blocks:]
-    output = torch.cat([class_token for _, class_token in intermediate_output], dim=-1)
-    if use_avgpool:
-        output = torch.cat(
-            (
-                output,
-                torch.mean(intermediate_output[-1][0], dim=1),  # patch tokens
-            ),
-            dim=-1,
-        )
-        output = output.reshape(output.shape[0], -1)
-    return output.float()
-
-
 class LinearRegressor(nn.Module):
     """Linear layer to train on top of frozen features"""
 
--- dinov2/eval/utils.py
+++ dinov2/eval/utils.py
@@ -18,11 +18,25 @@ from dinov2.data import DictDatasetWithEnumeratedTargets, SamplerType, make_data
 import dinov2.distributed as distributed
 from dinov2.logging import MetricLogger
 from dinov2.eval.segmentation_3d.vit_adapter import ViTAdapter
-from dinov2.eval.linear3d_reg import create_linear_input
 
 
 logger = logging.getLogger("dinov2")
 Reduce = Literal["mean", "max", "median"]
+
+
+def create_linear_input(x_tokens_list, use_n_blocks, use_avgpool):
+    intermediate_output = x_tokens_list[-use_n_blocks:]
+    output = torch.cat([class_token for _, class_token in intermediate_output], dim=-1)
+    if use_avgpool:
+        output = torch.cat(
+            (
+                output,
+                torch.mean(intermediate_output[-1][0], dim=1),  # patch tokens
+            ),
+            dim=-1,
+        )
+        output = output.reshape(output.shape[0], -1)
+    return output.float()
 
 
 class ModelWithNormalize(torch.nn.Module):
